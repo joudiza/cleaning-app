3f6a4064e26b4908ffba7b4e0f0521905b8591fc
joudizza <awatifelidrissi1995@gmail.com>
2025-07-07 22:04:56 +0100

    ✅ Create build.sh

---
+++ backend/build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+pip install -r requirements.txt
+python manage.py migrate
+python manage.py loaddata statuses.json
+python manage.py loaddata rooms.json
+python manage.py collectstatic --noinput
