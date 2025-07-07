b981c2204a12364aef347f6877e5ceb9f07adb2f
joudizza <awatifelidrissi1995@gmail.com>
2025-07-07 19:36:09 +0100

    fix structure for deployment

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-m+k!d$_7gcy_@k@vsoftq)wlv%3gd7b%#ym-k)(ah5xxv1s2#d'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
