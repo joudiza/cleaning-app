5570ecb3730efb4a889aeb3c32d1d6fba51f7d4d
joudizza <awatifelidrissi1995@gmail.com>
2025-07-07 21:12:58 +0100

    👤 Add create_admin script for Render

--- backend/create_admin.py
+++ backend/create_admin.py
@@ -1,11 +1,21 @@
 # create_admin.py
 
+import os
+import django
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
+django.setup()
+
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
 if not User.objects.filter(username="joud").exists():
-    User.objects.create_superuser("joud", "awatifelidrissi1995@", "AhmedJoud2022@")
-    print("✅ Joud created")
+    User.objects.create_superuser(
+        username="joud",
+        email="awatifelidrissi1995@gmail.com",
+        password="AhmedJoud2022@"
+    )
+    print("✅ Admin user created")
 else:
-    print("⚠️ Joud already exists")
+    print("⚠️ Admin user already exists")
