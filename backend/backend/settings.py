1ed8aa75d61b4195a418a8d00d53bf0baee22377
joudizza <awatifelidrissi1995@gmail.com>
2025-07-08 20:40:04 +0100

    Fix static asset path

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -153,7 +153,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'frontend', 'dist', 'assets'),  # ← هنا الملفات ديال React
+    os.path.join(BASE_DIR, 'frontend', 'dist'),  # ← هنا الملفات ديال React
 ]
 
 TEMPLATES[0]["DIRS"] = [
