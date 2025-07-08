0f6793bb72b9d0247a17404eedde3c61c578036b
joudizza <awatifelidrissi1995@gmail.com>
2025-07-09 00:25:33 +0100

    update buil.sh

--- backend/build.sh
+++ backend/build.sh
@@ -1,7 +1,11 @@
-#!/bin/bash
 
-pip install -r requirements.txt
-python manage.py migrate
+
+cd frontend 
+npm install 
+npm run build 
+cd .. 
+pip install -r requirements.txt 
+python manage.py migrate 
 python manage.py loaddata statuses.json
 python manage.py loaddata rooms.json
 python manage.py collectstatic --noinput
