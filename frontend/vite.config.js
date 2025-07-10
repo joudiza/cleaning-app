40fea551c06dbd85895a05a695801df94dd5abf0
joudizza <awatifelidrissi1995@gmail.com>
2025-07-10 01:24:34 +0100

    add responsive design

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,35 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+import { VitePWA } from 'vite-plugin-pwa'
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+     VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Hotel Room Tracker',
+        short_name: 'DEL_SITJAR_ROOMS',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#ffffff',
+        theme_color: '#000000',
+        icons: [
+          {
+            src: 'logo192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'logo512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
   base: '/static/', // ✅ مهم بزاف فـ الإنتاج باش static files يخدمو مع Django
   build: {
-    outDir: '../frontend/dist', // ✅ خاص يكون ف نفس المستوى مع backend/templates
+    outDir: '../backend/static', // ✅ خاص يكون ف نفس المستوى مع backend/templates
     emptyOutDir: true,
     manifest: true, // ✅ ضروري باش Django يعرف فين الملفات
     rollupOptions: {
