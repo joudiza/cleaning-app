d8c372443fa51ebb59bf9a10fa9668a99862ff46
joudizza <awatifelidrissi1995@gmail.com>
2025-07-11 22:57:42 +0100

    add new feild: is-available

--- backend/rooms/models.py
+++ backend/rooms/models.py
@@ -9,6 +9,7 @@ class RoomStatus(models.Model):
 class Room(models.Model):
     number = models.CharField(max_length=10)
     status = models.ForeignKey(RoomStatus, on_delete=models.SET_NULL, null=True)
+    is_available = models.BooleanField(default=True)
 
     def __str__(self):
-        return f"Room {self.number} - {self.status.name if self.status else 'بدون حالة'}"
+        return f"Room {self.number} - {self.status.name if self.status else 'without status '} - {'availabale' if self.is_available else 'occupied'}"
